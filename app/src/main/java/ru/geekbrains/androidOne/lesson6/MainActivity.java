9cff7b3819814db8fd4fe5e840aa2cdd6d14aaad
HomchenkoP <homchenkop@yandex.ru>
2021-03-25 19:50:10 +0300

    2. Создайте боковое навигационное меню для своего приложения и добавьте туда хотя бы один экран, например «Настройки» или «О приложении».

--- app/src/main/java/ru/geekbrains/androidOne/lesson6/MainActivity.java
+++ app/src/main/java/ru/geekbrains/androidOne/lesson6/MainActivity.java
@@ -1,14 +1,20 @@
 package ru.geekbrains.androidOne.lesson6;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 // 1. ... Используйте подход Single Activity для отображения экранов.
 
@@ -36,12 +42,39 @@ public class MainActivity extends AppCompatActivity {
                         .commit();
             }
         }
-        initToolbar();
+        Toolbar toolbar = initToolbar();
+        initDrawer(toolbar);
     }
 
-    private void initToolbar() {
+    private Toolbar initToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        return toolbar;
+    }
+
+    // регистрация drawer
+    private void initDrawer(Toolbar toolbar) {
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Обработка навигационного меню
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (navigateFragment(id)){
+                    drawer.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
@@ -49,13 +82,20 @@ public class MainActivity extends AppCompatActivity {
         // Обработка выбора пункта меню приложения (активити)
         int id = item.getItemId();
 
+        if (navigateFragment(id)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private boolean navigateFragment(int id) {
         switch (id) {
             case R.id.action_settings:
                 //addFragment(new SettingsFragment());
                 Toast.makeText(MainActivity.this, "TODO Открыть окно настроек", Toast.LENGTH_SHORT).show();
                 return true;
         }
-        return super.onOptionsItemSelected(item);
+        return false;
     }
 
     @Override
